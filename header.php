9e173ac946bad9fa043cd6574973c6cbdcc52969
Fenix Solis <fsolisg@gmail.com>
2018-04-30 22:21:03 -0600

    ciclo de posts
    còmo se hace los ciclos de posts

--- header.php
+++ header.php
@@ -6,7 +6,19 @@
     <title>Super Tema</title>
     <?php wp_head(); ?><!-- aquí es donde wordpress agrega todo lo que yo modifico del tema.--> 
 </head>
-<body>
+
+
+<?php
+    /* is_home busca la pàgina de blog */
+    if(is_front_page()):
+        $clases_body = array('super-clase', 'mi-clase');
+    else:
+        $clases_body = array('no-super-clase', 'no-mi-clase');
+    endif;
+?>
+
+
+<body <?php body_class($clases_body);?>>
 
     <?php wp_nav_menu(array('theme_location' => 'primary')); ?> 
  
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header();?>
 <h1>Este es el index.php de mi tema.</h1>
+
+    <?php 
+    if( have_posts() ):
+        while( have_posts() ): the_post(); ?>
+
+           <h3><?php the_title(); ?></h3> 
+           <small>Publicado el: <?php the_time( 'F j, y')?>, la categoria: <?php the_category( ) ?> </small>
+
+            <p><?php the_content(); ?></p>
+
+            <hr>
+    <?php endwhile;
+    endif;
+    ?>
+
+
+
 <?php get_footer();?>
